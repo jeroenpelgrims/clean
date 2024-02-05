8de1da661aace268e4d5035c64e214e598501327
Jeroen Pelgrims <github.com@jeroenpelgrims.com>
2024-02-05 13:49:01 +0100

    Allow separate development and production workflows

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,14 +3,9 @@ import { createClient, type Client } from '@libsql/client';
 import { LibSQLDatabase, drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
 
-// export const client = createClient({
-// 	url: env.TURSO_DB_URL ?? '',
-// 	authToken: env.TURSO_DB_AUTH_TOKEN,
-// });
-// export const db = drizzle(client, { schema });
-
 export let client: Client;
 let db: LibSQLDatabase<typeof schema>;
+
 export function connect() {
 	if (!db) {
 		client = createClient({
