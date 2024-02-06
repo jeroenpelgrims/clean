e67d7f1fd508ab55fc2676db2f4f067ac897206d
Jeroen Pelgrims <github.com@jeroenpelgrims.com>
2024-02-06 15:06:14 +0100

    Add migrations step

--- drizzle/migrate.ts
+++ drizzle/migrate.ts
@@ -1,7 +1,13 @@
+import { createClient } from '@libsql/client';
 import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/libsql/driver';
 import { migrate } from 'drizzle-orm/libsql/migrator';
-import { client, connect } from '../src/lib/db/index';
+import * as schema from '../src/lib/db/schema';
 
-const db = connect();
+const client = createClient({
+	url: process.env.TURSO_DB_URL ?? '',
+	authToken: process.env.TURSO_DB_AUTH_TOKEN,
+});
+const db = drizzle(client, { schema });
 await migrate(db, { migrationsFolder: './drizzle/migrations' });
 client.close();
